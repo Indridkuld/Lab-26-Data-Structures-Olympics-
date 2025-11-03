f2c4a79eb46800204ec2553f0642da6dcb46f064
Aidan <aidanmichaelwoodcock@gmail.com>
2025-11-02 18:31:29 -0800

    update

--- main.cpp
+++ main.cpp
@@ -37,7 +37,7 @@ int main() {
                                 tmp.push_back(cd);
                         auto end = chrono::high_resolution_clock::now();
                         auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
-                        results[0][i] = duration.count();
+                        results[0][0][i] = duration.count();
                         break;
                     }
                     case 1: {  // read into a list
@@ -45,7 +45,7 @@ int main() {
                                 data_list.push_back(cd);
                         auto end = chrono::high_resolution_clock::now();
                         auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
-                        results[0][i] = duration.count();
+                        results[0][0][i] = duration.count();
                         break;
                     }
                     case 2: {  // read into a set
@@ -53,7 +53,7 @@ int main() {
                                 data_set.insert(cd);
                         auto end = chrono::high_resolution_clock::now();
                         auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
-                        results[0][i] = duration.count();
+                        results[0][0][i] = duration.count();
                         break;
                     }
                 }
@@ -65,21 +65,23 @@ int main() {
                 auto start = chrono::high_resolution_clock::now();
                 switch(i) {
                     case 0: {  // sort a vector
-                        sort(data_vector.begin(), data_vector.end());
+                        vector<string> tmp = data_vector;
+                        sort(tmp.begin(), tmp.end());
                         auto end = chrono::high_resolution_clock::now();
                         auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
-                        results[1][i] = duration.count();
+                        results[0][1][i] = duration.count();
                         break;
                     }
-                    case 1: {  // sort a list
+                    case 1: {  // sort a 
+                        list<string> tmp(data_list.begin(), data_list.end());
                         data_list.sort();
                         auto end = chrono::high_resolution_clock::now();
                         auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
-                        results[1][i] = duration.count();
+                        results[0][1][i] = duration.count();
                         break;
                     }
                     case 2: {  // can't sort a set, so set to -1
-                        results[1][i] = -1;
+                        results[0][1][i] = -1;
                         break;
                     }
                 }
@@ -87,24 +89,28 @@ int main() {
 
             // testing for INSERT operations
             for (int i = 0; i < STRUCTURES; i++) {
-                int ind_v = data_vector.size() / 2;
-                int ind_l = data_list.size() / 2;
                 auto start = chrono::high_resolution_clock::now();
                 switch(i) {
                     case 0: {  // insert into a vector
+                        vector<string> tmp = data_vector;
+                        size_t ind = tmp.size() / 2;
+                
                         data_vector.insert(data_vector.begin() + ind_v, "TESTCODE");
                         auto end = chrono::high_resolution_clock::now();
                         auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
-                        results[2][i] = duration.count();
+                        results[0][2][i] = duration.count();
                         break;
                     }
                     case 1: {  // insert into a list
-                        auto it = data_list.begin();
-                        advance(it, ind_l);
+                        list<string> tmp(data_list.begin(), data_list.end());
+
+                        auto it = tmp.begin();
+                        advance(it, tmp.size() / 2);
                         data_list.insert(it, "TESTCODE");
+
                         auto end = chrono::high_resolution_clock::now();
                         auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
-                        results[2][i] = duration.count();
+                        results[0][2][i] = duration.count();
                         break;
                     }
                     case 2: {  // insert into a set
